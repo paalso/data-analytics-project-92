a7857658e5a3f55b9bf3e97ef7b00c16d1a59501
Pavel Soroka <40a.pvl@gmail.com>
2024-01-26 10:39:16 +0200

    UPD salespersons' queries and result

--- queries.sql
+++ queries.sql
@@ -1,12 +1,14 @@
 -- The the total number of customers
+-- The output: -- The result: top_10_total_income.csv
 select COUNT(*) as customers_count from customers;
 
 
 -- The 10 top-performing employees based on total income
+-- The output: top_10_total_income.csv
 SELECT
     CONCAT(e.first_name, ' ', e.last_name) AS name,
     COUNT(*) AS operations,
-    ROUND(SUM(s.quantity * p.price), 0) AS income
+    floor(SUM(s.quantity * p.price)) AS income
 FROM sales s
 JOIN employees e ON e.employee_id = s.sales_person_id
 JOIN products p ON p.product_id = S.product_id
@@ -16,6 +18,7 @@ LIMIT 10;
 
 
 -- The employees whose individual average operation income is below the total average income
+-- The output: lowest_average_income.csv
 WITH
 	sale_incoms AS (
 	    SELECT
@@ -30,7 +33,7 @@ WITH
 	)
 SELECT
     sale_incoms.name,
-    ROUND(AVG(income), 0) AS average_income
+    FLOOR(AVG(income)) AS average_income
 FROM sale_incoms
 CROSS JOIN total_average_income
 GROUP BY sale_incoms.name, total_average_income.average_income
