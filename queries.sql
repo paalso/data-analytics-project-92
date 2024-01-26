658dbb6be0093af566edd6924e51841614f9a7a0
Pavel Soroka <40a.pvl@gmail.com>
2024-01-26 12:07:56 +0200

    Add day_of_the_week_income report

--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
--- The the total number of customers
--- The output: -- The result: top_10_total_income.csv
+-- The total number of customers
+-- Output: -- The result: top_10_total_income.csv
 SELECT COUNT(*) as customers_count from customers;
 
 
 -- The 10 top-performing employees based on total income
--- The output: top_10_total_income.csv
+-- Output: top_10_total_income.csv
 SELECT
     CONCAT(e.first_name, ' ', e.last_name) AS name,
     COUNT(*) AS operations,
@@ -18,7 +18,7 @@ LIMIT 10;
 
 
 -- The employees whose individual average operation income is below the total average income
--- The output: lowest_average_income.csv
+-- Output: lowest_average_income.csv
 WITH
 	sale_incoms AS (
 	    SELECT
@@ -41,3 +41,21 @@ HAVING AVG(income) < total_average_income.average_income
 ORDER BY average_income;
 
 
+-- The income grouped by days of week and employees
+-- Output: day_of_the_week_income.csv
+WITH reformatted_sales AS (
+    SELECT
+        CONCAT(e.first_name, ' ', e.last_name) AS name,
+        TO_CHAR(sale_date, 'FMday') AS weekday,
+        TO_CHAR(sale_date, 'ID') AS weekday_id,
+        ROUND(SUM(s.quantity * p.price), 0) AS income
+    FROM sales s
+    JOIN employees e ON e.employee_id = s.sales_person_id
+    JOIN products p ON p.product_id = S.product_id
+    GROUP by
+    	CONCAT(e.first_name, ' ', e.last_name), weekday, weekday_id
+    ORDER BY weekday_id, name
+)
+SELECT
+	name, weekday, income
+FROM reformatted_sales;
